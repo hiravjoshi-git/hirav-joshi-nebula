320bf44c42af42174b84a5a4fbe379a4df75b463
Hirav Joshi <hiravjoshi4@gmail.com>
2025-08-20 15:56:08 +0530

    changes for now to build the code for vercel to update on deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-
 export default defineConfig(({ mode }) => ({
-  root: "src", // 👈 tell Vite your root folder
   build: {
-    outDir: "../dist", // 👈 place build output outside src
+    outDir: "dist", // keep dist in root
+    emptyOutDir: true,
   },
   server: {
     host: "::",
