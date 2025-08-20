7b289ba22e76692034ce6f55eb1d24a4c879f319
Hirav Joshi <hiravjoshi4@gmail.com>
2025-08-20 15:31:10 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,36 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
 export default defineConfig(({ mode }) => ({
+  root: "src", // 👈 tell Vite your root folder
+  build: {
+    outDir: "../dist", // 👈 place build output outside src
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
